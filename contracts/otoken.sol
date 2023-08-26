076c49c2d7f759ef836967bf2dd554924c9aa992
brasmus182 <bigbadbeard@tutanota.com>
2023-08-26 13:35:59 -0500

    committing psuedocode

---
+++ contracts/otoken.sol
@@ -0,0 +1,44 @@
+pragma solidity ^0.8.13;
+
+//bring in proper libraries for inheritance and security
+
+contract otoken {
+
+    //establish proper usages for overflows and what not 
+    //ERC20 stuff, fixedpoint math stuff
+
+    //Import custom errors
+    //Custom errors save on gas and since we shouldn't have a crazy number of types of errors we can simply write custom errors...
+    //instead of using requires which use more gas
+
+    //Setup Events
+    //Here we'll set up the proper events that we want to use in the token
+    //Looks like an "exericise", "Set Oracle", and "Set Treasury" event so these can be listened to/monitored on chain
+
+    //set up immutables
+    //These would include tokenadmin, payment token, and the underlying token
+    //As well as the public treasury and other storage variables (oracle)
+
+    //Constructor
+    //Setup everything needed
+
+    //Set name of token, etc
+
+    /////////////////////////////////////////////////////////////////////////////////////////////// 
+    /// Functions
+    /////////////////////////////////////////////////////////////////////////////////////////////// 
+    
+    //mint function
+
+
+    //exercise function
+
+    //set oracle function
+
+    //set treasury function
+
+    //set internal exercise function
+
+    
+
+}
